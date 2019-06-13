fd4389f5902e7fad8317790e5472a942034b2b71
JP Lomas <jp.lomas@gmail.com>
2019-06-13 10:38:16 +0100

    Add QRLAddressFromEPKHex method

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
+var sha256 = require("js-sha256");
+
+function hexToBytes(hex) {
+  for (var bytes = [], c = 0; c < hex.length; c += 2)
+    bytes.push(parseInt(hex.substr(c, 2), 16));
+  return bytes;
+}
+
+function bytesToHex(bytes) {
+  for (var hex = [], i = 0; i < bytes.length; i++) {
+    hex.push((bytes[i] >>> 4).toString(16));
+    hex.push((bytes[i] & 0xf).toString(16));
+  }
+  return hex.join("");
+}
+
+function hashPKandformat(ePK){
+  const h = hexToBytes(ePK);
+  return sha256(h);
+}
+
+function doConvert(ePK) {
+  const a = hashPKandformat(ePK);
+  const qx = sha256(hexToBytes('000400' + a));
+  const qm = qx.slice(56, 64);
+  return `Q${bytesToHex([0,4,0])}${a}${qm}`;
+}
+
 module.exports = {
-  /**
-   * version: reports current version
-   */
+ /**
+  * Reports the current module version
+  * @return {string} version
+  */
   version: function() {
-    return '0.0.2'
+    return '1.0.0'
+  },
+  QRLAddressFromEPKHex: function (ePK) {
+    if (ePK === undefined) {
+      throw new Error('No ePK parameter')
+    }
+    if (ePK.length !== 134) {
+      throw  new Error('ePK length invalid')
+    }
+    return doConvert(ePK)
   }
 };
--- test/test.js
+++ test/test.js
@@ -2,11 +2,26 @@ var expect = require('chai').expect;
 var assert = require('chai').assert;
 var helpers = require('../index.js');
 
-describe('#helpers', function() {
-
+describe('#deploy', function() {
   it(`.version should report same version as in npm package.json file (=${process.env.npm_package_version})`, function() {
     var result = helpers.version();
     expect(result).to.equal(process.env.npm_package_version);
   });
+});
 
+describe('#QRLAddressFromEPKHex', function() {
+  it('calling with no parameter throws an error', function () {
+    expect(() => {
+      helpers.QRLAddressFromEPKHex()
+    }).to.throw();
+  })
+});
+
+describe('#QRLAddressFromEPKHex', function() {
+  it('test ePK results in result of Q000400846365cd097082ce4404329d143959c8e4557d19b866ce8bf5ad7c9eb409d036651f62bd', function () {
+    var result = helpers.QRLAddressFromEPKHex(
+    '000400106D0856A5198967360B6BDFCA4976A433FA48DEA2A726FDAF30EA8CD3FAD2113191DA3442686282B3D5160F25CF162A517FD2131F83FBF2698A58F9C46AFC5D'
+    );
+    expect(result).to.equal('Q000400846365cd097082ce4404329d143959c8e4557d19b866ce8bf5ad7c9eb409d036651f62bd');
+  })
 });
