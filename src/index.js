ce71d6f682fd98e6f64ff7d1f1cc8db3f7fd2612
JP Lomas <jp@theqrl.org>
2026-02-12 18:18:40 +0000

    fix: isDeepStrictEqual -> custom sameKeys fn
    Signed-off-by: JP Lomas <jp@theqrl.org>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 var sha256 = require('js-sha256')
-var isEqual = require('util').isDeepStrictEqual
+function sameKeys(expected, actual) {
+  if (expected.length !== actual.length) return false
+  var set = new Set(actual)
+  return expected.every(function(k) { return set.has(k) })
+}
 var v3WalletHelpers = require('./v3wallet')
 
 function hexToBytes(hex) {
@@ -82,14 +86,14 @@ function getWalletFileType(wallet) {
   const webWallet = ['address', 'addressB32', 'pk', 'hexseed', 'mnemonic', 'height', 'hashFunction', 'signatureType', 'index', 'encrypted']
   const pythonNode = ['addresses', 'encrypted', 'version']
   const convertedWebWallet = ['pk', 'hexseed', 'mnemonic', 'height', 'hashFunction', 'signatureType', 'index', 'address', 'encrypted']
-  if (isEqual(pythonNode, Object.keys(wallet))) {
+  if (sameKeys(pythonNode, Object.keys(wallet))) {
     return 'PYTHON-NODE'
   }
   if (wallet instanceof Array) {
-    if (isEqual(webWallet, Object.keys(wallet[0]))) {
+    if (sameKeys(webWallet, Object.keys(wallet[0]))) {
       return 'WEB-WALLET'
     }
-    if (isEqual(convertedWebWallet, Object.keys(wallet[0]))) {
+    if (sameKeys(convertedWebWallet, Object.keys(wallet[0]))) {
       return 'CONVERTED-WEB-WALLET'
     }
   }
